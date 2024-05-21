2a572c70dc4f878963bd9f28e5c01964974d42c3
ThanhNganDev <thanhngan.7170@gmail.com>
2024-05-21 14:51:49 +0700

    SinhVien

---
+++ SinhVien/src/main/java/com/example/SinhVien/SinhVienApplication.java
@@ -0,0 +1,13 @@
+package com.example.SinhVien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinhVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinhVienApplication.class, args);
+	}
+
+}
---
+++ SinhVien/src/test/java/com/example/SinhVien/SinhVienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SinhVien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinhVienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
